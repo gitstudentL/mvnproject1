2108aa59d028d31b63f0d3ed4b625bd6a0296774
Lslstudent <Lslstudent@qq.com>
2023-09-27 17:02:47 +0800

    hotfix commit

--- src/main/java/com/lsl/App.java
+++ src/main/java/com/lsl/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println( "Hello World!33" );
         System.out.println("hot-fix");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
 
     }
 }
