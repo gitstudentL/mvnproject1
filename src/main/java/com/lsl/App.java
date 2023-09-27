cdb2c32ecee8e585a5cf2f03273f7bb728d97764
Lslstudent <Lslstudent@qq.com>
2023-09-27 18:02:02 +0800

    master commit3

--- src/main/java/com/lsl/App.java
+++ src/main/java/com/lsl/App.java
@@ -16,6 +16,8 @@ public class App
         System.out.println("hot-fix");
         System.out.println("mastertest");
         System.out.println("hot-fixtest");
+        System.out.println("mastertest");
+        System.out.println("hot-fixtest");
 
 
     }
